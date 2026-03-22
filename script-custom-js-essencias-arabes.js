1f3dc320c2f35a8f8618b69fea3ea4c98cb39276
unknown <lochaydeguerreiro@hotmail.com>
2026-03-22 13:16:54 -0300

    Commit 23

--- script-custom-js-essencias-arabes.js
+++ script-custom-js-essencias-arabes.js
@@ -1,8 +1,11 @@
 (function() {
+
     const style = document.createElement('style');
+    style.id = 'style-ocultar-elementos';
     style.innerHTML = `
         .alert.-yellow, 
-        .section-pages .holder-content .pages { 
+        .section-pages .holder-content .pages,
+        .category-options > div { 
             display: none !important; 
         }
     `;
@@ -10,11 +13,15 @@
 
     function gerenciarVisibilidade() {
         const urlAtual = window.location.href;
+
         if (!urlAtual.includes("sobre-nos")) {
-            style.innerHTML = '';
+            const cssInjetado = document.getElementById('style-ocultar-elementos');
+            if (cssInjetado) {
+                cssInjetado.remove();
+            }
         }
     }
-    
+
     if (document.readyState === 'loading') {
         document.addEventListener('DOMContentLoaded', gerenciarVisibilidade);
     } else {
