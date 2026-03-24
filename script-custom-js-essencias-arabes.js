1e2cfceec16d3370579f8c34408edfa0f93db3bd
unknown <lochaydeguerreiro@hotmail.com>
2026-03-24 11:05:56 -0300

    Commit 33

--- script-custom-js-essencias-arabes.js
+++ script-custom-js-essencias-arabes.js
@@ -1,30 +1,30 @@
-(function() {
+// (function() {
 
-    const style = document.createElement('style');
-    style.id = 'style-ocultar-elementos';
-    style.innerHTML = `
-        .alert.-yellow, 
-        .section-pages .holder-content .pages,
-        .category-options > div { 
-            display: none !important; 
-        }
-    `;
-    document.head.appendChild(style);
+//     const style = document.createElement('style');
+//     style.id = 'style-ocultar-elementos';
+//     style.innerHTML = `
+//         .alert.-yellow, 
+//         .section-pages .holder-content .pages,
+//         .category-options > div { 
+//             display: none !important; 
+//         }
+//     `;
+//     document.head.appendChild(style);
 
-    function gerenciarVisibilidade() {
-        const urlAtual = window.location.href;
+//     function gerenciarVisibilidade() {
+//         const urlAtual = window.location.href;
 
-        if (!urlAtual.includes("sobre-nos")) {
-            const cssInjetado = document.getElementById('style-ocultar-elementos');
-            if (cssInjetado) {
-                cssInjetado.remove();
-            }
-        }
-    }
+//         if (!urlAtual.includes("sobre-nos")) {
+//             const cssInjetado = document.getElementById('style-ocultar-elementos');
+//             if (cssInjetado) {
+//                 cssInjetado.remove();
+//             }
+//         }
+//     }
 
-    if (document.readyState === 'loading') {
-        document.addEventListener('DOMContentLoaded', gerenciarVisibilidade);
-    } else {
-        gerenciarVisibilidade();
-    }
-})();
+//     if (document.readyState === 'loading') {
+//         document.addEventListener('DOMContentLoaded', gerenciarVisibilidade);
+//     } else {
+//         gerenciarVisibilidade();
+//     }
+// })();
